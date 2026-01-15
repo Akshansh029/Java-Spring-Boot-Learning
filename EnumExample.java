331724c714145c50ed6a671e42d5650079501a72
Akshansh <akshanshsingh00@gmail.com>
2026-01-15 16:28:34 +0530

    Inner classes and Enums

--- EnumExample.java
+++ EnumExample.java
@@ -1,7 +1,25 @@
-public class EnumExample {
-      public enum Day{
-            Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+enum Laptop {
+      MacBook(2000), Asus(1200), XPS(1800), ThinkPad(1600), HP;
+      
+      private int price;
+
+      private Laptop(){
+            price = 1000;
       }
+
+      private Laptop(int price){
+            this.price = price;
+      }
+
+      public int getPrice(){
+            return price;
+      }
+}
+enum Day{
+      Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+}
+public class EnumExample {
+
 
       public static void main(String[] args) {
             Day yesterday = Day.Sunday;
@@ -11,5 +29,10 @@ public class EnumExample {
             System.out.println("Yesterday was " + yesterday);
             System.out.println("Today is " + today);
             System.out.println("Tomorrow will be " + tomorrow);
+
+            Laptop[] laptops = Laptop.values();
+            for(Laptop l : laptops){
+                  System.out.println(l + " : " + l.getPrice());
+            }
       }
 }
--- quizApp/QuestionService.java
+++ quizApp/QuestionService.java
@@ -1,6 +1,8 @@
 package quizApp;
+import java.util.Scanner;
 
 public class QuestionService {
+      private int score = 0;
       Question[] questions = new Question[5];
 
       public QuestionService(){
@@ -13,5 +15,35 @@ public class QuestionService {
             questions[3] = new Question(4, "Which access modifier makes a member accessible only within the same class?", "public", "protected", "default", "private", "private");
 
             questions[4] = new Question(5, "Which concept allows a child class to provide a specific implementation of a method?", "Abstraction", "Encapsulation", "Method Overloading", "Method Overriding", "Method Overriding");
+      }
+
+      public void incScore(){
+            score++;
+      }
+
+      public String displayScore(){
+            return String.format("Your final score is %d/5!\nThank you for playing our quiz app!", score);
+      }
+
+      public void playQuiz(){
+            try (Scanner sc = new Scanner(System.in)) {
+                  System.out.println("Welcome to QuizApp! Let's start the quiz...\n");
+                  for (Question question : questions) {
+                        System.out.println(question.toString());
+                        System.out.printf("\nProvide your answer: ");
+                        String answer = sc.nextLine();
+                        if (question.checkAnswer(question, answer.trim())) {
+                            incScore();
+                            System.out.println("\nCorrect answer! ");
+                        } else {
+                            System.out.println("\nWrong answer, the correct option is " + question.getAnswer());
+                        }
+                        System.out.println();
+                  }
+                  System.out.println(displayScore());
+                  sc.close();
+            } catch(Exception e){
+                  System.out.println(e.getMessage());
+            }
       }
 }
