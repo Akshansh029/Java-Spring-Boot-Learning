2111ada6f9478bcb81dcb9cc2f56fa8260c07596
Akshansh <akshanshsingh00@gmail.com>
2026-02-02 10:19:17 +0530

    Expense Tracker- List command

--- projects/simple-expense-tracker/src/main/java/com/akshansh/commands/ListExpenses.java
+++ projects/simple-expense-tracker/src/main/java/com/akshansh/commands/ListExpenses.java
@@ -1,7 +1,11 @@
 package com.akshansh.commands;
 
+import com.akshansh.ExpenseDAO;
+import com.akshansh.classes.Expense;
 import picocli.CommandLine;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(
@@ -11,6 +15,27 @@ import java.util.concurrent.Callable;
 public class ListExpenses implements Callable<Integer> {
     @Override
     public Integer call(){
+        try{
+            ExpenseDAO dao = new ExpenseDAO();
+            List<Expense> expenseList = dao.getExpensesList();
 
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+
+            if(expenseList.isEmpty()){
+                System.out.println("No expenses yet!");
+            } else{
+                System.out.println("ID        Date            Amount        Description");
+                expenseList.forEach(e -> {
+                    System.out.println(e.getId() + "       " + formatter.format(e.getCreatedAt()) +
+                            "      Rs." + e.getAmount() + "        "
+                            + e.getDescription());
+                });
+            }
+
+            return 0;
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+            return 1;
+        }
     }
 }
