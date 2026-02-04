254a616049fc6b4c2e61bbbc0263152514b070a4
Akshansh <akshanshsingh00@gmail.com>
2026-02-04 14:26:58 +0530

    SpringJDBCExample- Connected with Postgresql

--- projects/SpringJDBCExample/src/main/java/com/akshansh/springjdbcexample/SpringJdbcExampleApplication.java
+++ projects/SpringJDBCExample/src/main/java/com/akshansh/springjdbcexample/SpringJdbcExampleApplication.java
@@ -15,8 +15,8 @@ public class SpringJdbcExampleApplication {
         ApplicationContext context = SpringApplication.run(SpringJdbcExampleApplication.class, args);
 
         Student s1 = context.getBean(Student.class);
-        s1.setRollNo(104);
-        s1.setName("Akshansh");
+        s1.setRollNo(102);
+        s1.setName("Sourabh");
         s1.setMarks(95);
 
         StudentService service = context.getBean(StudentService.class);
